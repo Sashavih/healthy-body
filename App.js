04af6fd3fd75449ef9baca157c4934aee615c3ee
Sasha <sashavikhorevaa@gmail.com>
2023-10-24 00:35:09 +0300

    feat : new text

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>HELLO</Text>
+      <Text>FUCK YOU BITCH</Text>
       <StatusBar style="auto" />
     </View>
   );
